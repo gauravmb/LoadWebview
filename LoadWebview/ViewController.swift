e0009f4bfa176d080cdfde2ef7686c8940f387a9
gauravmb <gauravmbhatia@gmail.com>
2017-05-10 13:22:12 +0530

    Initial Commit

---
+++ LoadWebview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadWebview
+//
+//  Created by Gaurav Bhatia on 10/05/17.
+//  Copyright © 2017 Gaurav Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
